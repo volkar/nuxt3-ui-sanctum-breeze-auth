5cf26d0b683124e97ba20bce42c3a8636bcf5727
Sergey Volkar <sergey@volkar.ru>
2024-06-16 17:14:35 +0300

    Favicons and related meta tags added to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,14 +52,41 @@ export default defineNuxtConfig({
 					href: "/favicon.ico",
 				},
 				{
-					rel: "shortcut icon",
-					type: "image/x-icon",
-					href: "/favicon.ico",
+					rel: "apple-touch-icon",
+					sizes: "180x180",
+					href: "/apple-touch-icon.png",
 				},
 				{
-					rel: "apple-touch-icon",
-					type: "image/x-icon",
-					href: "/favicon.ico",
+					rel: "icon",
+					type: "image/png",
+					sizes: "32x32",
+					href: "/favicon-32x32.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "16x16",
+					href: "/favicon-16x16.png",
+				},
+				{
+					rel: "mask-icon",
+					href: "/safari-pinned-tab.svg",
+					color: "#5bbad5",
+				},
+				{
+					rel: "manifest",
+					href: "/site.webmanifest",
+				},
+			],
+
+			meta: [
+				{
+					name: "msapplication-TileColor",
+					content: "#ffffff",
+				},
+				{
+					name: "theme-color",
+					content: "#ffffff",
 				},
 			],
 		},
